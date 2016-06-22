b02c555222094d0458e33e88d5af44035a0244f1
dbreyton <david.breyton@atos.net>
2016-06-22 18:05:43 +0200

    Fix #10268 upgrade mongo searching with text operator

--- src/main/java/net/atos/entng/collaborativewall/events/CollaborativeWallSearchingEvents.java
+++ src/main/java/net/atos/entng/collaborativewall/events/CollaborativeWallSearchingEvents.java
@@ -44,19 +44,15 @@ public class CollaborativeWallSearchingEvents implements SearchingEvents {
 	@Override
 	public void searchResource(List<String> appFilters, String userId, JsonArray groupIds, JsonArray searchWords, Integer page, Integer limit,
 							   final JsonArray columnsHeader, final String locale, final Handler<Either<String, JsonArray>> handler) {
-		final List<String> returnFields = new ArrayList<String>();
-		returnFields.add("name");
-		returnFields.add("description");
-		returnFields.add("modified");
-		returnFields.add("owner.userId");
-		returnFields.add("owner.displayName");
-
-		final List<String> searchFields = new ArrayList<String>();
-		searchFields.add("name");
-		searchFields.add("description");
-
 		if (appFilters.contains(CollaborativeWallSearchingEvents.class.getSimpleName())) {
-			searchService.search(userId, groupIds.toList(), returnFields, searchWords.toList(), searchFields, page, limit, new Handler<Either<String, JsonArray>>() {
+			final List<String> returnFields = new ArrayList<String>();
+			returnFields.add("name");
+			returnFields.add("description");
+			returnFields.add("modified");
+			returnFields.add("owner.userId");
+			returnFields.add("owner.displayName");
+
+			searchService.search(userId, groupIds.toList(), returnFields, searchWords.toList(), page, limit, new Handler<Either<String, JsonArray>>() {
 				@Override
 				public void handle(Either<String, JsonArray> event) {
 					if (event.isRight()) {
