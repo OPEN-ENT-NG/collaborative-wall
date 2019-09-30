c4fe531614e3ade64f9df9714315a3886dec3b1e
Valentin PEIRO <valentin.peiro@opendigitaleducation.com>
2019-09-30 11:53:01 +0200

    feat(archive): #25940, Aerate portions of the export code

--- src/main/java/net/atos/entng/collaborativewall/service/CollaborativeWallRepositoryEvents.java
+++ src/main/java/net/atos/entng/collaborativewall/service/CollaborativeWallRepositoryEvents.java
@@ -44,58 +44,83 @@ public class CollaborativeWallRepositoryEvents extends MongoDbRepositoryEvents {
 
     @Override
     public void exportResources(String exportId, String userId, JsonArray groups, String exportPath,
-                                String locale, String host, Handler<Boolean> handler) {
+                                String locale, String host, Handler<Boolean> handler)
+    {
         QueryBuilder findByAuthor = QueryBuilder.start("owner.userId").is(userId);
         QueryBuilder findByShared = QueryBuilder.start().or(
-                QueryBuilder.start("shared.userId").is(userId).get(),
-                QueryBuilder.start("shared.groupId").in(groups).get());
+            QueryBuilder.start("shared.userId").is(userId).get(),
+            QueryBuilder.start("shared.groupId").in(groups).get()
+        );
         QueryBuilder findByAuthorOrShared = QueryBuilder.start().or(findByAuthor.get(),findByShared.get());
         JsonObject query = MongoQueryBuilder.build(findByAuthorOrShared);
+
         final AtomicBoolean exported = new AtomicBoolean(false);
-        mongo.find(CollaborativeWall.COLLABORATIVE_WALL_COLLECTION, query, new Handler<Message<JsonObject>>() {
+
+        mongo.find(CollaborativeWall.COLLABORATIVE_WALL_COLLECTION, query, new Handler<Message<JsonObject>>()
+        {
             @Override
-            public void handle(Message<JsonObject> event) {
+            public void handle(Message<JsonObject> event)
+            {
                 JsonArray results = event.body().getJsonArray("results");
-                if ("ok".equals(event.body().getString("status")) && results != null) {
-                    results.forEach(elem -> {
+                if ("ok".equals(event.body().getString("status")) && results != null)
+                {
+                    results.forEach(elem ->
+                    {
                         JsonObject wall = ((JsonObject) elem);
                         wall.put("name","wall_" + wall.getString("name"));
                     });
+
                     final Set<String> ids = results.stream().map(res -> ((JsonObject)res).getString("_id")).collect(Collectors.toSet());
                     QueryBuilder findByWallId = QueryBuilder.start("idwall").in(ids);
                     JsonObject query2 = MongoQueryBuilder.build(findByWallId);
-                    mongo.find(CollaborativeWall.COLLABORATIVE_WALL_NOTES_COLLECTION, query2, new Handler<Message<JsonObject>>() {
+
+                    mongo.find(CollaborativeWall.COLLABORATIVE_WALL_NOTES_COLLECTION, query2, new Handler<Message<JsonObject>>()
+                    {
                         @Override
                         public void handle(Message<JsonObject> event2) {
                             JsonArray results2 = event2.body().getJsonArray("results");
-                            if ("ok".equals(event2.body().getString("status")) && results2 != null) {
-                                results2.forEach(elem -> {
+                            if ("ok".equals(event2.body().getString("status")) && results2 != null)
+                            {
+                                results2.forEach(elem ->
+                                {
                                     JsonObject note = ((JsonObject) elem);
                                     note.put("name","note_" + note.getString("_id"));
                                 });
-                                createExportDirectory(exportPath, locale, new Handler<String>() {
+
+                                createExportDirectory(exportPath, locale, new Handler<String>()
+                                {
                                     @Override
-                                    public void handle(String path) {
-                                        if (path != null) {
-                                            exportDocumentsDependancies(results.addAll(results2), path, new Handler<Boolean>() {
+                                    public void handle(String path)
+                                    {
+                                        if (path != null)
+                                        {
+                                            exportDocumentsDependancies(results.addAll(results2), path, new Handler<Boolean>()
+                                            {
                                                 @Override
-                                                public void handle(Boolean bool) {
+                                                public void handle(Boolean bool)
+                                                {
                                                     exportFiles(results, path, new HashSet<String>(), exported, handler);
                                                 }
                                             });
 
-                                        } else {
+                                        }
+                                        else
+                                        {
                                             handler.handle(exported.get());
                                         }
                                     }
                                 });
-                            } else {
+                            }
+                            else
+                            {
                                 log.error("Blog : Could not proceed query " + query2.encode(), event2.body().getString("message"));
                                 handler.handle(exported.get());
                             }
                         }
                     });
-                } else {
+                }
+                else
+                {
                     log.error("Blog : Could not proceed query " + query.encode(), event.body().getString("message"));
                     handler.handle(exported.get());
                 }
