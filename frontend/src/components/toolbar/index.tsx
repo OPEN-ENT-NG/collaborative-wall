5503e6c4cd6526e24d2e49fd4f5e45113e780211
Headan <lucien.devarsdumayne@opendigitaleducation.com>
2024-01-31 15:10:18 +0100

    feat: #WB2-1253, add component Toolbar to create tollbar whiteboard

--- frontend/src/components/toolbar/index.tsx
+++ frontend/src/components/toolbar/index.tsx
@@ -1,17 +1,22 @@
+import { useMemo } from "react";
+
 import {
   Plus,
   PointerDefault,
   PointerHand,
+  Redo,
   Undo,
   ZoomIn,
   ZoomOut,
 } from "@edifice-ui/icons";
+import { Toolbar, ToolbarItem } from "@edifice-ui/react";
+import { useTranslation } from "react-i18next";
 import { useShallow } from "zustand/react/shallow";
 
 import { useWhiteboard } from "../../hooks/useWhiteBoard";
 import { zoomConfig } from "~/config/init-config";
 
-export const Toolbar = ({
+export const ToolbarWrapper = ({
   zoomIn,
   zoomOut,
   resetTransform,
@@ -26,7 +31,6 @@ export const Toolbar = ({
     canMoveBoard,
     canMoveNote,
     createNote,
-    resetZoom,
     setCanMoveBoard,
     setCanMoveNote,
     toggleCanMoveBoard,
@@ -36,51 +40,152 @@ export const Toolbar = ({
       canMoveBoard: state.canMoveBoard,
       canMoveNote: state.canMoveNote,
       createNote: state.createNote,
-      resetZoom: state.resetZoom,
       setCanMoveBoard: state.setCanMoveBoard,
       setCanMoveNote: state.setCanMoveNote,
       toggleCanMoveBoard: state.toggleCanMoveBoard,
       toggleCanMoveNote: state.toggleCanMoveNote,
-      toggleCanZoom: state.toggleCanZoom,
     })),
   );
 
-  return (
-    <div className="toolbar">
-      <button onClick={() => resetTransform(zoomConfig.DEFAULT_ZOOM)}>
-        Centrer
-      </button>
-      <button>
-        <Undo />
-      </button>
-      <button
-        style={{ backgroundColor: canMoveNote && "#E5F5FF" }}
-        onClick={() => {
-          toggleCanMoveNote();
-          setCanMoveBoard(false);
-        }}
-      >
-        <PointerDefault />
-      </button>
-      <button
-        style={{ backgroundColor: canMoveBoard && "#E5F5FF" }}
-        onClick={() => {
-          toggleCanMoveBoard();
-          setCanMoveNote(false);
-        }}
-      >
-        <PointerHand />
-      </button>
-      <button onClick={() => zoomOut(zoomConfig.SCALE_ZOOM)}>
-        <ZoomOut />
-      </button>
-      <button onClick={resetZoom}>{(newZoom * 100).toFixed(0) + "%"}</button>
-      <button onClick={() => zoomIn(zoomConfig.SCALE_ZOOM)}>
-        <ZoomIn />
-      </button>
-      <button onClick={createNote}>
-        <Plus />
-      </button>
-    </div>
-  );
+  const { t } = useTranslation();
+
+  const WhiteboardItems: ToolbarItem[] = useMemo(() => {
+    return [
+      {
+        type: "icon",
+        name: "undo",
+        props: {
+          icon: <Undo />,
+          "aria-label": t("undo"),
+          color: "tertiary",
+          onClick: () => console.log("undo"),
+        },
+        tooltip: {
+          message: t("undo"),
+          position: "top",
+        },
+      },
+      {
+        type: "icon",
+        name: "redo",
+        props: {
+          icon: <Redo />,
+          "aria-label": t("undo"),
+          color: "tertiary",
+          onClick: () => console.log("redo"),
+        },
+        tooltip: {
+          message: t("redo"),
+          position: "top",
+        },
+      },
+      {
+        type: "divider",
+        name: "div-1",
+      },
+      {
+        type: "icon",
+        name: "pointerDefault",
+        props: {
+          icon: <PointerDefault />,
+          "aria-label": t("move note"),
+          color: "tertiary",
+          className: canMoveNote ? "is-selected" : "",
+          onClick: () => {
+            toggleCanMoveNote();
+            setCanMoveBoard(false);
+          },
+        },
+        tooltip: {
+          message: t("move note"),
+          position: "top",
+        },
+      },
+      {
+        type: "icon",
+        name: "pointerHand",
+        props: {
+          icon: <PointerHand />,
+          "aria-label": t("move whiteboard"),
+          color: "tertiary",
+          className: canMoveBoard ? "is-selected" : "",
+          onClick: () => {
+            toggleCanMoveBoard();
+            setCanMoveNote(false);
+          },
+        },
+        tooltip: {
+          message: t("move whiteboard"),
+          position: "top",
+        },
+      },
+      {
+        type: "divider",
+        name: "div-2",
+      },
+      {
+        type: "icon",
+        name: "zoomOut",
+        props: {
+          icon: <ZoomOut />,
+          "aria-label": t("zoom out"),
+          color: "tertiary",
+          onClick: () => zoomOut(zoomConfig.SCALE_ZOOM),
+        },
+        tooltip: {
+          message: t("zoom out"),
+          position: "top",
+        },
+      },
+      {
+        type: "button",
+        name: "center",
+        props: {
+          "aria-label": t("center"),
+          color: "tertiary",
+          children: (newZoom * 100).toFixed(0) + "%",
+          onClick: () => resetTransform(zoomConfig.DEFAULT_ZOOM),
+        },
+        tooltip: {
+          message: t("center"),
+          position: "top",
+        },
+      },
+      {
+        type: "icon",
+        name: "zoomIn",
+        props: {
+          icon: <ZoomIn />,
+          "aria-label": t("zoom in"),
+          color: "tertiary",
+          onClick: () => zoomIn(zoomConfig.SCALE_ZOOM),
+        },
+        tooltip: {
+          message: t("zoom in"),
+          position: "top",
+        },
+      },
+      {
+        type: "divider",
+        name: "div-3",
+      },
+      {
+        type: "icon",
+        name: "create",
+        props: {
+          "aria-label": t("create"),
+          leftIcon: <Plus />,
+          variant: "filled",
+          color: "secondary",
+          onClick: createNote,
+        },
+        tooltip: {
+          message: t("create"),
+          position: "top",
+        },
+      },
+    ];
+  }, [t, canMoveBoard, canMoveNote, newZoom]);
+
+  return <Toolbar className="p-8" items={WhiteboardItems} />;
 };
