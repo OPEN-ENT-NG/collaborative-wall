9a8c737f91364ddb0de07ecbb62eb3d1c19406cb
Nabil Mansouri <nabil.mansouri.3@gmail.com>
2024-02-08 17:35:23 +0100

    feat: #WB2-1310, integrate to EUR

--- backend/src/main/java/net/atos/entng/collaborativewall/CollaborativeWall.java
+++ backend/src/main/java/net/atos/entng/collaborativewall/CollaborativeWall.java
@@ -21,13 +21,19 @@ package net.atos.entng.collaborativewall;
 
 import net.atos.entng.collaborativewall.controllers.CollaborativeWallController;
 import net.atos.entng.collaborativewall.events.CollaborativeWallSearchingEvents;
+import net.atos.entng.collaborativewall.explorer.WallExplorerPlugin;
 import net.atos.entng.collaborativewall.service.CollaborativeWallRepositoryEvents;
 import net.atos.entng.collaborativewall.service.NoteService;
 import net.atos.entng.collaborativewall.service.impl.MongoDbNoteService;
+import org.entcore.common.explorer.IExplorerPluginClient;
+import org.entcore.common.explorer.impl.ExplorerRepositoryEvents;
 import org.entcore.common.http.BaseServer;
 import org.entcore.common.http.filter.ShareAndOwner;
 import org.entcore.common.mongodb.MongoDbConf;
 import org.entcore.common.service.impl.MongoDbSearchService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Server to manage collaborative walls. This class is the entry point of the Vert.x module.
@@ -35,23 +41,33 @@ import org.entcore.common.service.impl.MongoDbSearchService;
  */
 public class CollaborativeWall extends BaseServer {
 
+    public static final String APPLICATION = "collaborativewall";
+    public static final String TYPE = "wall";
+    public static final String NOTE_TYPE = "note";
     /**
      * Constant to define the MongoDB collection to use with this module.
      */
     public static final String COLLABORATIVE_WALL_COLLECTION = "collaborativewall";
     public static final String COLLABORATIVE_WALL_NOTES_COLLECTION = "collaborativewall.notes";
-
+    private WallExplorerPlugin plugin;
     /**
      * Entry point of the Vert.x module
      */
     @Override
     public void start() throws Exception {
         super.start();
-        setRepositoryEvents(new CollaborativeWallRepositoryEvents(vertx));
+        // wrap repository event
+        final IExplorerPluginClient mainClient = IExplorerPluginClient.withBus(vertx, APPLICATION, TYPE);
+        final Map<String, IExplorerPluginClient> pluginClientPerCollection = new HashMap<>();
+        pluginClientPerCollection.put(COLLABORATIVE_WALL_COLLECTION, mainClient);
+        pluginClientPerCollection.put(COLLABORATIVE_WALL_NOTES_COLLECTION, IExplorerPluginClient.withBus(vertx, APPLICATION, NOTE_TYPE));
+        setRepositoryEvents(new ExplorerRepositoryEvents(new CollaborativeWallRepositoryEvents(vertx), pluginClientPerCollection,mainClient));
+
         if (config.getBoolean("searching-event", true)) {
             setSearchingEvents(new CollaborativeWallSearchingEvents(new MongoDbSearchService(COLLABORATIVE_WALL_COLLECTION)));
         }
 
+        this.plugin = WallExplorerPlugin.create(securedActions);
         MongoDbConf conf = MongoDbConf.getInstance();
         conf.setCollection(COLLABORATIVE_WALL_COLLECTION);
         conf.setResourceIdLabel("id");
@@ -60,7 +76,17 @@ public class CollaborativeWall extends BaseServer {
 
         setDefaultResourceFilter(new ShareAndOwner());
 
-        addController(new CollaborativeWallController(COLLABORATIVE_WALL_COLLECTION, noteService));
+        addController(new CollaborativeWallController(COLLABORATIVE_WALL_COLLECTION, noteService, this.plugin));
+
+        this.plugin.start();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+        if(this.plugin != null){
+            this.plugin.stop();
+        }
     }
 
 }
