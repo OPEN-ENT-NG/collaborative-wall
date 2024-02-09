770e0a23dd562877c9624818f4259e238dd09fc8
Clement Creusat <clement.creusat@gmail.com>
2024-02-09 09:09:41 +0100

    fix: add temporary root page

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,10 +1,46 @@
-import { RouteObject, createBrowserRouter } from "react-router-dom";
+import { odeServices } from "edifice-ts-client";
+import {
+  Link,
+  RouteObject,
+  createBrowserRouter,
+  useLoaderData,
+} from "react-router-dom";
 
+import { CollaborativeWallProps } from "./collaborative-wall";
 import ErrorPage from "~/components/page-error";
 
 import "~/styles/index.css";
 
+/* TEMPORARY */
+function Root() {
+  const data = useLoaderData() as CollaborativeWallProps[];
+
+  return (
+    <ul>
+      {data.map((wall) => {
+        return (
+          <li>
+            <Link to={`id/${wall._id}`}>{wall._id}</Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
 const routes: RouteObject[] = [
+  /* TEMPORARY */
+  {
+    path: "/",
+    loader: async () => {
+      const walls = await odeServices
+        .http()
+        .get<CollaborativeWallProps>(`/collaborativewall/list/all`);
+
+      console.log(walls);
+      return walls;
+    },
+    element: <Root />,
+  },
   {
     path: "id/:id",
     async lazy() {
