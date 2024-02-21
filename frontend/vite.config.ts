3c8c70654be323cec1170b31b21587f993443691
Clement Creusat <clement.creusat@gmail.com>
2024-02-21 15:05:50 +0100

    chore: add chunkSizeWarningLimit to vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -52,6 +52,7 @@ export default ({ mode }: { mode: string }) => {
 
   const build = {
     assetsDir: "public",
+    chunkSizeWarningLimit: 2000,
     rollupOptions: {
       external: ["edifice-ts-client"],
       output: {
