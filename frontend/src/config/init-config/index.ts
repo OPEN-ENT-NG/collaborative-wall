7004120164a84e188c35ba652f1bf77af690bf49
Headan <lucien.devarsdumayne@opendigitaleducation.com>
2024-03-19 12:53:42 +0100

    feat: #WB2-1380, add modify background color

--- frontend/src/config/init-config/index.ts
+++ frontend/src/config/init-config/index.ts
@@ -61,11 +61,18 @@ export const noteColors: Colors = {
   },
 };
 
-export const defaultBackground = "/img/cloud.png";
-
 export const backgroundImages = [
   "/img/cloud.png",
   "/img/default.jpg",
   "/img/paper.jpg",
   "/img/wood.jpg",
+];
+
+export const backgroundColors = [
+  "115deg, #E5F5FF 0.32%, #46AFE6 100%",
+  "116.76deg, #FFECEE 0.32%, #FF3A55 99.93%",
+  "116.76deg, #F6ECF9 0.32%, #A348C0 99.93%",
+  "116.76deg, #FFEFE3 0.32%, #FF8D2E 99.93%",
+  "116.76deg, #EAF7E4 0.32%, #5AC235 99.93%",
+  "116.76deg, #FBF4D5 0.32%, #F1CA00 99.93%",
 ];
