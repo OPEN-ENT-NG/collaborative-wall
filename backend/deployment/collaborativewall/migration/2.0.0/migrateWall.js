873b3f4478fcd24dcc32f609abd003032810136d
Nabil Mansouri <nabil.mansouri.3@gmail.com>
2024-04-17 17:21:04 +0200

    feat: #WB2-1553, migrate old wall

---
+++ backend/deployment/collaborativewall/migration/2.0.0/migrateWall.js
@@ -0,0 +1,22 @@
+var cursor = db.collaborativewall.find();
+var counter = 0
+while (cursor.hasNext()) {
+  var doc = cursor.next();
+  var oldBackground = doc.background;
+  if (typeof oldBackground !== "object") {
+    db.collaborativewall.update(
+      { _id: doc._id },
+      {
+        $set: {
+          background: {
+            path: "img/edifice.png",
+            color: "",
+          },
+          oldBackground: oldBackground,
+        },
+      }
+    );
+    counter++
+  }
+}
+print("Number of wall updated:"+counter)
