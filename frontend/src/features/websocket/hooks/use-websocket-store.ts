6212393bb7e882e6d506709cd41bc2e26f9d7d2f
Clement Creusat <clement.creusat@gmail.com>
2024-04-24 14:54:54 +0200

    refactor(zustand): use setState method to update websocket store

--- frontend/src/features/websocket/hooks/use-websocket-store.ts
+++ frontend/src/features/websocket/hooks/use-websocket-store.ts
@@ -1,9 +1,10 @@
+import { useEffect } from "react";
 import { createWebsocketStore } from "~/store/websocket/store";
 import { Mode, Status } from "~/store/websocket/types";
 import { useHttpMode } from "./use-http-mode";
 import { useWSMode } from "./use-ws-mode";
 
-export const useWebsocketStore = () => {
+/* export const useWebsocketStore = () => {
   const store = createWebsocketStore();
   const httpProvider = useHttpMode(store.mode === Mode.HTTP, store.resourceId);
 
@@ -29,5 +30,45 @@ export const useWebsocketStore = () => {
     reconnectAttempts: store.maxAttempts,
     wallId: store.resourceId,
   });
+  return store;
+}; */
+
+export const useWebsocketStore = () => {
+  const store = createWebsocketStore();
+  const httpProvider = useHttpMode(store.mode === Mode.HTTP, store.resourceId);
+  const wsProvider = useWSMode({
+    enabled: store.mode === Mode.WS,
+    onOpen() {
+      store.onReady(Mode.WS);
+    },
+    onClose() {
+      if (store.mode === Mode.WS && store.status === Status.STARTED) {
+        store.disconnect();
+      }
+    },
+    onMessage(event) {
+      if (store.mode === Mode.WS) {
+        store.subscribers.forEach((sub) => sub(event));
+      }
+    },
+    onReconnectStop() {
+      store.onReady(Mode.HTTP);
+    },
+    reconnectAttempts: store.maxAttempts,
+    wallId: store.resourceId,
+  });
+
+  useEffect(() => {
+    /* Correctly use setState from zustand to update the store */
+    const cleanup = createWebsocketStore.setState((state) => ({
+      ...state,
+      httpProvider: httpProvider,
+      wsProvider,
+    }));
+
+    return cleanup;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return store;
 };
