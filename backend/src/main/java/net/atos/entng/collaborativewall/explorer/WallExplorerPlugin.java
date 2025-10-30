87d25b07817f49a063e860dd06e78d96fde0b3eb
Junior Bernard <junior.bernard@opendigitaleducation.com>
2025-10-30 08:16:30 +0100

    feat: #RBACK-127 #RBACK-155, type exportResources result + async explorer

--- backend/src/main/java/net/atos/entng/collaborativewall/explorer/WallExplorerPlugin.java
+++ backend/src/main/java/net/atos/entng/collaborativewall/explorer/WallExplorerPlugin.java
@@ -33,11 +33,9 @@ public class WallExplorerPlugin extends ExplorerPluginResourceMongo {
     private ShareService shareService;
     private final Map<String, SecuredAction> securedActions;
 
-    public static WallExplorerPlugin create(final Map<String, SecuredAction> securedActions) throws Exception {
-        final IExplorerPlugin plugin = ExplorerPluginFactory.createMongoPlugin((params)->{
-            return new WallExplorerPlugin(params.getCommunication(), params.getDb(), securedActions);
-        });
-        return (WallExplorerPlugin) plugin;
+    public static Future<WallExplorerPlugin> create(final Map<String, SecuredAction> securedActions) {
+        return ExplorerPluginFactory.createMongoPlugin((params)-> new WallExplorerPlugin(params.getCommunication(), params.getDb(), securedActions))
+                .map(plugin -> (WallExplorerPlugin) plugin);
     }
 
     public WallExplorerPlugin(final IExplorerPluginCommunication communication, final MongoClient mongoClient, final Map<String, SecuredAction> securedActions) {
